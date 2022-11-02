5414cd20f2e0dd7d7199b4d44945ede456c8709a
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-11-02 16:05:52 +0100

    final

---
+++ src/reducers/products.js
@@ -0,0 +1,14 @@
+import {createSlice } from '@reduxjs/toolkit';
+
+const productData= [
+  {id:1, title:"Sock", price: "100", emoji:"🧦"},
+  {id:2, title:"Dress", price: "400", emoji:"👗"},
+  {id:3, title:"Pants", price: "300", emoji:"👖"},
+  {id:4, title:"Jacket", price: "500", emoji:"🧥"}
+]
+
+export const products= createSlice({
+  name: 'products',
+  initialState: productData
+})
+
